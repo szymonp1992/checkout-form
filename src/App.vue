4a4b37862c66b2f6e2aa68f7ebf6604948912278
Szymon Podstufka (Prywatne) <szymonpodstufka@MBP-Szymon.kopnet.pl>
2023-01-08 16:23:35 +0100

    Added Vue files and configured Bootstrap

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <form action="">
+      <label for="name" class="form-label">Name</label>
+      <div class="input-group">
+        <div class="input-group-text">Name:</div>
+        <input type="text" id="name" class="form-control" />
+        <div class="input-group-text">Surname:</div>
+        <input type="text" id="surname" class="form-control" />
+      </div>
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
+const app = createApp(App)
+
+app.use(bootstrap)
+
+app.mount('#app')
